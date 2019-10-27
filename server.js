f4f2f794f3ec7492d13a0657600a529982626372
chrobahs <47793145+chrobahs@users.noreply.github.com>
2019-10-26 22:15:27 -0500

    added database connection

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import connectDatabase from './config/db';
 const app = express();
 app.get('/',(req,res)=>
     res.send('http get request sent to root api endpoint')
