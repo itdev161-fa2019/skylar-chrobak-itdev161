6a903dde3327473eef1ed76907906db342a39bc3
chrobahs <47793145+chrobahs@users.noreply.github.com>
2019-10-27 00:02:53 -0500

    Completed Activity 3

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import { check, validationResult} from 'express-validator';
 const app = express();
+connectDatabase();
+app.use(express.json({extended:false}));
+/* @route
+@desc  */
+
 app.get('/',(req,res)=>
-    res.send('http get request sent to root api endpoint')
-    );
+    res.send('http get request sent to root api endpoint'));
+/*
+*@route
+*@desc
+*/
+app.post(
+    '/api/users', 
+    [
+        check('name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('Please enter a password with six or more characters'
+        ).isLength({min:6})
+    ],
+    (req,res)=>{
+        const errors = validationResults(req);
+        if (!errors.isEmpty()){
+            return res.status(422).json({errors:errors.array()});
+        } else {
+            return res.send(req.body);
+        }
+});
 app.listen(3000, () => console.log ('Express server running on port 3000'));
